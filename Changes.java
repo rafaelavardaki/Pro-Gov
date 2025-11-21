f5ba5b15476553cdebd5b5e0daaca1aa47593b7d
DimitraVoutsa <t8240019@aueb.gr>
2025-11-21 15:42:54 +0200

    arxikos kwdikas sta changes

--- Changes.java
+++ Changes.java
@@ -1,3 +1,27 @@
-public class Changes {
-    
+import java.util.Scanner;
+
+public class Changes extends Esoda1 {
+    do {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Σε ποιον κλάδο θα ήθελες να κάνεις αλλαγές;");
+        System.out.println("1.Έσοδα, 2.Έξοδα");
+        int epilogi = scanner.nextInt();
+        if (epilogi == 1) {
+            System.out.println("Ποια κατηγορία θα ήθελες να αλλάξεις;");
+            for (int i = 0 ; i < Category.lenght ; i++ ) {
+                System.out.println((i+1) + Category[i]);
+            }
+            int katigoria = scanner.nextInt();
+            System.out.println(Category[katigoria-1] + "-> ");
+            int timi = scanner.nextInt();
+            Value[katigoria-1] = timi;
+            System.out.println(Category[katigoria-1] + ":" + Value[katigoria-1]);
+
+        } else {
+            int y = 2;
+        }
+        System.out.println("Θα ήθελες να γίνει κάποια άλλη αλλαγή;");
+        System.out.println("1.Ναι, 2.Όχι");
+        int answer = scanner.nextInt();
+    } while (answer == 1);
 }
