3a4781daf45a83c971ffa5a6db0c544ec2ce7e4f
Rafaela Vardaki <t8240013@aueb.gr>
2025-11-23 18:30:13 +0200

    Checks are done

---
+++ Trial.java
@@ -0,0 +1,29 @@
+public class Trial {
+    public static void greet() {
+        System.out.println ("Δώσε τιμές στις παρακάτω κατηγορίες.");
+    }
+
+    public static void budgetItem(String[] a, int[] b) {
+        Trial.greet();
+        for (int i = 0; i < b.length; i ++) {
+            System.out.println(a[i] + " -> " );
+            b[i] = CheckVariables.checkingV();
+        }
+        System.out.println("Εμφάνιση στοιχείων ");
+        for (int i=0; i < b.length; i++) {
+            
+            System.out.println(a[i] + " : " + b[i]);
+        }
+    }
+
+    public static void comparison(int s1, int s2) {
+        if (s1 > s2) {
+            System.out.println("Ο κρατικός προϋπολογισμός είναι πλεονασματικός.");
+        } else if (s1 < s2) {
+            System.out.println("Ο κρατικός προϋπολογισμός είναι ελλειμματικός."); 
+        } else {
+            System.out.println("Ο κρατικός προϋπολογισμός είναι ισοσταθμισμένος.");
+        }
+    }
+
+}
